46dcda781b0704e524c7ec39a151f051d94b2d50
Luis Diego Torres <luis.torres.alvarez@cuc.cr>
2023-10-06 13:01:38 -0600

    End of section 6

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -11,7 +11,7 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AccountController : BaseApiController
+    public class AccountController : Controller
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
@@ -22,13 +22,12 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        public async Task<IActionResult> Register(RegisterDto registerDto)
         {
-            // if(await UserExists(registerDto.UserName))
-            // {
-
-            //     return await BadRequest("Username is taken"); //Error
-            // }
+            if(await UserExists(registerDto.UserName))
+            {
+                return  BadRequest("Username is taken"); //Error
+            }
 
             using var hmac = new HMACSHA512();
 
@@ -42,19 +41,16 @@ namespace API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return new UserDto
-            {
-                Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
-            };
+            return Ok(new UserDto{Username = user.UserName, Token = _tokenService.CreateToken(user)});
+
         }
 
         [HttpPost("login")]
-        public  async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        public  async Task<IActionResult> Login(LoginDto loginDto)
         {
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
-            //if(user is null) { return Unautorized("Invalid username."); }
+            if(user is null) { return Unauthorized("Invalid username."); }
 
             using var hmac = new HMACSHA512(user.PasswordSalt);
 
@@ -64,28 +60,14 @@ namespace API.Controllers
             {
                 if(computeHash[i] != user.PasswordHash[i])
                 {
-                    //return Unautorized("Error");
-                    return null;
+                    return Unauthorized("Invalid password.");
                 }
             }
 
-             return new UserDto
-            {
-                Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
-            };
+            return Ok(new UserDto {Username = user.UserName,Token = _tokenService.CreateToken(user)});
+
         }
 
-        private ActionResult<IActionResult> Unautorized(string v)
-        {
-            return Unautorized(v);
-        }
-
-        private  async Task<ActionResult> BadRequest(string v)
-        {
-            return await BadRequest(v);
-        }
-    
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { Observable, of } from 'rxjs';
-import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav', 
@@ -14,7 +14,10 @@ export class NavComponent implements OnInit{
   model: any = {};
   //currentUser$: Observable<User | null> = of(null); 
 
-  constructor(public accounService: AccountService){}
+  constructor(public accounService: AccountService, 
+              private router: Router,
+              private toastr: ToastrService
+  ){}
 
   ngOnInit():void{
     //this.currentUser$ = this.accounService.currentUser$;
@@ -22,15 +25,16 @@ export class NavComponent implements OnInit{
 
   login(){
     this.accounService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => console.log(error)
+
+      next: _ => this.router.navigateByUrl('/members'), //_ we're not using an argument for this
+      error: error => this.toastr.error(error.error)
+      
     })
   }
   
   logout(){
     this.accounService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
