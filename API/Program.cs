9c5f34356d1160e5a39ba5ee362601a7ba51b513
Luis Diego Torres Alvarez <diego@fitprotracker.com>
2024-11-06 20:11:42 -0600

    End section 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,13 +8,15 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyOrigin()
+     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
